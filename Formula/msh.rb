3463ea2fe260767ea861eff024f6ca5cbf2ca7d9
Max2 <38298972+Max2github@users.noreply.github.com>
2022-06-01 20:38:45 +0200

    Update msh.rb

--- Formula/msh.rb
+++ Formula/msh.rb
@@ -2,9 +2,9 @@ class msh < Formula
   desc "The msh Interpreter as shell program."
   homepage "https://github.com/Max2github/mshgit"
   url "https://github.com/Max2github/mshgit/archive/refs/tags/shell.tar.gz"
-  #sha256 ""
+  sha256 "04c277dec15b4417a8311fc5bae238ffd900b4c4b32505df8efd1a63c13d18ec"
   license "MIT"
-  #version "1.0.1"
+  version "Shell program"
 
   # depends_on "cmake" => :build
 
