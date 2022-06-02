faa33d907072b95c4c03e4ac84d520d0cc47b34b
Max2 <38298972+Max2github@users.noreply.github.com>
2022-06-02 07:09:39 +0200

    Update msh.rb

--- Formula/msh.rb
+++ Formula/msh.rb
@@ -14,7 +14,7 @@ class Msh < Formula
   def install
     #mkdir 'build' do
     system "cp other/mac_arm/all.o o/lib/all.o"
-    system "make dep=false shell"
+    system "make shell"
     system "make cleanshell"
     #bin.install "msh"
     #end
