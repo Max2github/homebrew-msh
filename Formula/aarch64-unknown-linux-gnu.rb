cf640457f877b7352d595457c522d5a736d908db
Max2 <38298972+Max2github@users.noreply.github.com>
2022-06-02 14:56:35 +0200

    Update aarch64-unknown-linux-gnu.rb

--- Formula/aarch64-unknown-linux-gnu.rb
+++ Formula/aarch64-unknown-linux-gnu.rb
@@ -4,7 +4,7 @@ class Aarch64UnknownLinuxGnu < Formula
     url "https://github.com/thinkski/osx-arm-linux-toolchains/releases/download/8.3.0/aarch64-unknown-linux-gnu.tar.xz"
     version "8.3.0"
     sha256 "9110fd08eb1fe1625fb7f495145a408db55efe6f9d6338bc4fd7a4c3057b86f6"
-    revision 1
+    #revision 1
     
     #depends_on macos
   
