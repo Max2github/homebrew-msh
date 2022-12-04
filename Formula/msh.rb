862530e41528f557ab1994e79de7347843111034
Max2 <38298972+Max2github@users.noreply.github.com>
2022-12-04 18:29:35 +0100

    Update msh.rb

--- Formula/msh.rb
+++ Formula/msh.rb
@@ -36,7 +36,7 @@ class Msh < Formula
     lsPrint()
     #system "setopt extendedglob"
     #system "mv * msh/mshgit"
-    FileUtils.mv Dir.glob("*"), "msh/mshgit"
+    FileUtils.mv(Dir.glob("*"), "msh/mshgit")
     #system "shopt -s extglob"
     #system "mv !(msh) msh/mshgit"
     #system "/usr/local/opt/coreutils/libexec/gnubin/mv !(msh) msh/mshgit"
